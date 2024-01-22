b30db7973cb2221bd666c2276b6302e7b00e1fb5
Lei Huang <thisisleihuang@gmail.com>
2024-01-22 01:46:36 +0000

    commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SharpeRatioAnalyzer',
+    version='1.0',
+    packages=find_packages(),
+    author='Lei H',
+    description='A short description of your package',
+    # url='https://github.com/yourusername/yourpackage',
+    # install_requires=[
+    #     # List your dependencies here
+    # ],
+)
